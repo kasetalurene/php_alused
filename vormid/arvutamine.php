ac9be6000661f4bdd811f8fbbc10fc26a330b67a
renekasetalu <rene.kasetalu@gmail.com>
2020-01-10 11:03:54 +0200

    Ülesanne 3

---
+++ vormid/arvutamine.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>06 - PHP - Vormid</title>
+</head>
+<body>
+<h1>Tellimine</h1>
+<form action="tellimine.php" method="get">
+    Silindri raadius <input type="text" name="sr"><br>
+    Toode 2 <input type="text" name="t2"><br>
+    Toode 3 <input type="text" name="t3"><br>
+    <input type="submit" value="Saada">
+</form>
+</body>
+</html><?php

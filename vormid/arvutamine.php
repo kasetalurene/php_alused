db1a940ef94e9d426374fbbc1395f9a841ec9896
renekasetalu <rene.kasetalu@gmail.com>
2020-03-07 14:02:02 +0200

    Vormid ja ülesanne 3

--- vormid/arvutamine.php
+++ vormid/arvutamine.php
@@ -1,3 +1,9 @@
+<!--
+ * file name: arvutamine.php;
+ * autor: Rene Kasetalu;
+ * date: 10.01.2020;
+-->
+
 <!doctype html>
 <html>
 <head>
@@ -5,12 +11,33 @@
     <title>06 - PHP - Vormid</title>
 </head>
 <body>
-<h1>Tellimine</h1>
-<form action="tellimine.php" method="get">
+<h1>Kujundid</h1>
+<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
     Silindri raadius <input type="text" name="sr"><br>
-    Toode 2 <input type="text" name="t2"><br>
-    Toode 3 <input type="text" name="t3"><br>
+    Silindri kõrgus <input type="text" name="sk"><br>
+    <!--    Toode 2 <input type="text" name="t2"><br>-->
+    <!--    Toode 3 <input type="text" name="t3"><br>-->
     <input type="submit" value="Saada">
+    <hr>
 </form>
 </body>
-</html><?php
+</html>
+<?php
+function silindriRuumala($raadius, $korgus){
+    $pohjaPindala = pi() * $raadius * $raadius;
+    $ruumala = $pohjaPindala * $korgus;
+    return $ruumala;
+}
+echo '<pre>';
+print_r($_GET);
+echo '</pre>';
+
+$sr = $_GET['sr'];
+$sk = $_GET['sk'];
+
+if($sr == '' or $sk == ''){
+    echo 'Tuleb sisestada kõik andmed!<br>';
+} else {
+    $silindiRuumala = silindriRuumala($sr, $sk);
+    echo 'Silinder raadiusega '.$sr.' ja kõrgusega '.$sk.' on ruumalaga '.$silindiRuumala.'<br>';
+}
--- vormid/tellimine.php
+++ vormid/tellimine.php
@@ -1,3 +1,19 @@
+<!--
+ * file name: tellimine.php;
+ * autor: Rene Kasetalu;
+ * date: 10.01.2020;
+-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Tellimine</h1>
 <?php
 //lisab vormist saadud andmed muutujasse
 $toode1 = $_GET['t1'];
@@ -8,3 +24,5 @@ echo 'Toode 1: '.$toode1.'tk<br>';
 echo 'Toode 2: '.$toode2.'tk<br>';
 echo 'Toode 3: '.$toode3.'tk<br>';
 ?>
+</body>
+</html>
